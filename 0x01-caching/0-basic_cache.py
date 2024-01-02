a42442e0cf074c6d4b817aa59c3879029664ac78
EvansOdhams <evansodhams@gmail.com>
2024-01-02 20:05:43 +0300

    Initial Commit

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,41 +1,23 @@
-#!/usr/bin/env python3
-""" BasicCache module. """
+#!/usr/bin/python3
+"""Basic Cache implementation Class
+"""
+BaseCaching = __import__('base_caching').BaseCaching
 
-class BaseCaching:
-    """ BaseCaching defines:
-      - constants of your caching system
-      - where your data are stored (in a dictionary)
-    """
-    MAX_ITEMS = 4
-
-    def __init__(self):
-        """ Initialize """
-        self.cache_data = {}
-
-    def print_cache(self):
-        """ Print the cache """
-        print("Current cache:")
-        for key in sorted(self.cache_data.keys()):
-            print("{}: {}".format(key, self.cache_data.get(key)))
-
-    def put(self, key, item):
-        """ Add an item in the cache """
-        raise NotImplementedError("put must be implemented in your cache class")
-
-    def get(self, key):
-        """ Get an item by key """
-        raise NotImplementedError("get must be implemented in your cache class")
 
 class BasicCache(BaseCaching):
-    """ BasicCache inherits from BaseCaching and is a simple caching system. """
+    """
+    A basic cache implementaion class
 
+    Attributes:
+        MAX_ITEMS: number of items that can be store in the cache
+    """
     def put(self, key, item):
-        """ Assign the item value for the key key in the cache_data dictionary. """
+        """ Add an item in the cache
+        """
         if key is not None and item is not None:
-            self.cache_data[key] = item
+            self.cache_data.update({key: item})
 
     def get(self, key):
-        """ Return the value linked to key in cache_data. """
-        if key is not None and key in self.cache_data:
-            return self.cache_data[key]
-        return None
+        """ Get an item by key
+        """
+        return self.cache_data.get(key, None)
